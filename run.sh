a009733fff0e3fc70b4523b09f6f057fda1f4946
Marc Schmid <business@marcschmid.ch>
2020-01-24 23:25:53 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ docker run -ti --rm --name icecast2-run \
     -p 8000:8000 \
     -v "$PWD/icecast.xml:/etc/icecast2/icecast.xml:ro" \
     -v "$PWD/silence.mp3:/usr/share/icecast2/web/silence.mp3:ro" \
-     m4rc/icecast2:latest
+     m4rc77/icecast2-docker:latest
